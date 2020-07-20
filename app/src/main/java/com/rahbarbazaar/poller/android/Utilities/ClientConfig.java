fc54641692b3cc5ea4c2a8a5e1d5cbe80bf8ef8e
mosayeb_masoumi <mosayeb.masoumi.co@gmail.com>
2020-07-20 18:32:13 +0430

    first commmit

--- app/src/main/java/com/rahbarbazaar/poller/android/Utilities/ClientConfig.java
+++ app/src/main/java/com/rahbarbazaar/poller/android/Utilities/ClientConfig.java
@@ -8,4 +8,5 @@ public class ClientConfig {
     public final static String API_V2 = "v2";
     public final static String HTML_ADDRESS = "http://pollerws.rahbarbazaar.com:2296/poller/";
 //    public final static String HTML_ADDRESS = "http://test.rahbarbazar.com/poller/";
+    
 }
